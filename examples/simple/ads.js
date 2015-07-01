dd8cdabc461e406994d0563c2932fcfa2b92a5c3
Ashik Sujath <ashiksujath2@gmail.com>
2015-07-01 18:41:31 +0530

    flash player integrated to simple example

--- examples/simple/ads.js
+++ examples/simple/ads.js
@@ -1,22 +1,10 @@
-/**
- * Copyright 2014 Google Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 var player = videojs('content_video');
 
+google.ima.settings.setVpaidAllowed(true);
+
 var options = {
+  debug: true,
   id: 'content_video',
   adTagUrl: 'http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&' +
       'iu=/124319096/external/ad_rule_samples&ciu_szs=300x250&ad_rule=1&' +
@@ -25,28 +13,24 @@ var options = {
       'vid=short_onecue&correlator='
 };
 
+options.adTagUrl = 'http://ad4.liverail.com/?LR_PUBLISHER_ID=45449&LR_SCHEMA=vast2-vpaid' +
+                    '&LR_VERTICALS=philly_rail_philadelphia_sports&LR_VIDEO_ID=__item-test_video' +
+                    '&LR_AUTOPLAY=1&LR_MUTED=1&LR_TAGS=rail,philly,&LR_VIDEO_AMID=philadelphia_sports' +
+                    '&LR_FORMAT=application/x-shockwave-flash;application/javascript';
+
 player.ima(options);
 
-// Remove controls from the player on iPad to stop native controls from stealing
-// our click
-var contentPlayer =  document.getElementById('content_video_html5_api');
-if ((navigator.userAgent.match(/iPad/i) ||
-      navigator.userAgent.match(/Android/i)) &&
-    contentPlayer.hasAttribute('controls')) {
-  contentPlayer.removeAttribute('controls');
-}
+vast_url = "http://ad3.liverail.com/?LR_PUBLISHER_ID=1331&LR_CAMPAIGN_ID=229&LR_SCHEMA=vast2";
 
-// Initialize the ad container when the video player is clicked, but only the
-// first time it's clicked.
-var startEvent = 'click';
-if (navigator.userAgent.match(/iPhone/i) ||
-    navigator.userAgent.match(/iPad/i) ||
-    navigator.userAgent.match(/Android/i)) {
-  startEvent = 'tap';
-}
+old_fun = player.ima.onAdError_;
 
-player.one(startEvent, function() {
-    player.ima.initializeAdDisplayContainer();
-    player.ima.requestAds();
-    player.play();
-});
+player.ima.onAdError_ = function(adErrorEvent) {
+  var flash = document.getElementById("SdkIntegration");
+  console.log("load flash ad");
+  flash.requestAds(options.adTagUrl);
+  // old_fun(adErrorEvent);
+};
+
+player.ima.requestAds();
+
+player.play();
